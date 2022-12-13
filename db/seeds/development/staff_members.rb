c17852ff9fb47222a66c645d6bd47fa7dc20564f
itoshusu <sopfantasy@yahoo.co.jp>
2022-12-14 03:00:17 +0900

    add: password周りを修正

--- db/seeds/development/staff_members.rb
+++ db/seeds/development/staff_members.rb
@@ -1,20 +1,43 @@
 StaffMember.create!(
-  email: "taro@example.com",
+  email: "taroa@example.com",
   family_name: "山田",
   given_name: "太郎",
   family_name_kana: "ヤマダ",
   given_name_kana: "タロウ",
   password: "password",
+  password_confirmation: "password",
   start_date: Date.today
 )
 
-StaffMember.create!(
-  email: "rota@example.com",
-  family_name: "山田",
-  given_name: "郎太",
-  family_name_kana: "ヤマダ",
-  given_name_kana: "ロタ",
-  password: "password",
-  start_date: Date.today,
-  suspended: true
-)
+family_names = %w{
+  佐藤:サトウ:sato
+  鈴木:スズキ:suzuki
+  高橋:タカハシ:takahashi
+  田中:タナカ:tanaka
+}
+
+given_names = %w{
+  二郎:ジロウ:jiro
+  三郎:サブロウ:saburo
+  松子:マツコ:matsuko
+  竹子:タケコ:takeko
+  梅子:ウメコ:umeko
+}
+
+20.times do |n|
+  fn = family_names[n % 4].split(":")
+  gn = given_names[n % 5].split(":")
+
+  StaffMember.create!(
+    email: "#{fn[2]}.#{gn[2]}@example.com",
+    family_name: fn[0],
+    given_name: gn[0],
+    family_name_kana: fn[1],
+    given_name_kana: gn[1],
+    password: "password",
+    password_confirmation: "password",
+    start_date: (100 - n).days.ago.to_date,
+    end_date: n == 0 ? Date.today : nil,
+    suspended: n == 1
+  )
+end

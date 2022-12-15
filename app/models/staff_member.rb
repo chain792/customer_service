5944f456b77a5119db73ab22c869bff3d0e0bebf
itoshusu <sopfantasy@yahoo.co.jp>
2022-12-16 00:58:01 +0900

    add: event機能追加

--- app/models/staff_member.rb
+++ app/models/staff_member.rb
@@ -1,6 +1,8 @@
 class StaffMember < ApplicationRecord
   authenticates_with_sorcery!
 
+  has_many :events, class_name: "StaffEvent", dependent: :destroy
+
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }

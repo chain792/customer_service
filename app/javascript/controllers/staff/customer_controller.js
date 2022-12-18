5bdbd4f99bc229bb26b6440c61d15ef8b09d8ba1
itoshusu <sopfantasy@yahoo.co.jp>
2022-12-18 23:59:52 +0900

    staff/customerフォーム

---
+++ app/javascript/controllers/staff/customer_controller.js
@@ -0,0 +1,24 @@
+import { Controller } from "@hotwired/stimulus"
+import jQuery from "jquery"
+window.$ = jQuery
+
+// Connects to data-controller="staff--customer"
+export default class extends Controller {
+  static targets = ["home", "work"]
+
+  initialize() {
+    this.toggle_home()
+    this.toggle_work()
+  }
+  toggle_home() {
+    const checked = this.homeTarget.checked
+    $("fieldset#home-address-fields input").prop("disabled", !checked);
+    $("fieldset#home-address-fields select").prop("disabled", !checked);
+  }
+  toggle_work() {
+    const checked = this.workTarget.checked
+    $("fieldset#work-address-fields input").prop("disabled", !checked);
+    $("fieldset#work-address-fields select").prop("disabled", !checked);
+  }
+
+}

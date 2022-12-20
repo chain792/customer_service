031436fd0b108757abce5bf3049ba913cef0163e
itoshusu <sopfantasy@yahoo.co.jp>
2022-12-20 22:51:22 +0900

    add: 検索機能追加

--- app/controllers/staff/customers_controller.rb
+++ app/controllers/staff/customers_controller.rb
@@ -1,6 +1,9 @@
 class Staff::CustomersController < Staff::Base
+  include StringNormalizer
+
   def index
-    @customers = Customer.order(:family_name_kana, :given_name_kana).page(params[:page])
+    @q = Customer.ransack(normalize_search_params)
+    @customers = @q.result.order(:family_name_kana, :given_name_kana).page(params[:page])
   end
 
   def show
@@ -45,4 +48,16 @@ class Staff::CustomersController < Staff::Base
     flash.notice = "顧客アカウントを削除しました。"
     redirect_to :staff_customers
   end
+
+  private
+
+  def normalize_search_params
+    p = params[:q]
+    p[:family_name_kana_eq] = normalize_as_furigana(p[:family_name_kana_eq])
+    p[:given_name_kana_eq] = normalize_as_furigana(p[:given_name_kana_eq])
+    p[:addresses_city_eq] = normalize_as_name(p[:addresses_city_eq])
+    p[:addresses_postal_code_eq] = normalize_as_postal_code(p[:addresses_postal_code_eq])
+    p[:phones_number_for_index_eq] = normalize_as_phone_number(p[:phones_number_for_index_eq]).try(:gsub, /\D/, "")
+    p
+  end 
 end
